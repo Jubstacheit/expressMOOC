649e542659acde17331d0164ce478b0de841cb2e
Viviane <dessaintviviane@gmail.com>
2023-07-08 14:16:18 +0200

    Chapter 14

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.use(express.static(path.join(__dirname, './phantom')));
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'));
 
+app.locals.siteName = 'Boîte à musique';
+app.locals.socials = [
+    {url: "https://www.twitter.com", class: "fa-twitter", label: "Twitter"},
+    {url: "https://www.facebook.com", class: "fa-facebook-f", label: "Facebook"},
+    {url: "https://www.instagram.com", class: "fa-instagram", label: "Instagram"},
+    {url: "https://www.github.com", class: "fa-github", label: "Github"},
+];
+
 app.use((req, res, next) => {
     console.log(`Time : ${Date.now()}, ${req.method} ${req.url}`)
     next();
